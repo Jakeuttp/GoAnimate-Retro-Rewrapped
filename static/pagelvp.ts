81e0f356b18b18128a45e2b74ae191d34b03107c
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-09-09 15:01:48 -0400

    Create pagelvp.ts

--- static/pagelvp.ts
+++ static/pagelvp.ts
@@ -757,6 +757,7 @@ function googleTranslateElementInit() {
   new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
 }
 </script>
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 <script>
 var IE = {
   Version: function() {
